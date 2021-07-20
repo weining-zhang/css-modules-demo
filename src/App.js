98cb74d6bbb6b9317f9beda866e6c9193fe06708
welnee <445103710@qq.com>
2021-07-20 15:27:21 +0800

    feat: add support for css module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import style from'./App.css';
 import Button from './Button';
 
 function App() {
   return (
     <>
-      <h1 className={'title'}>
+      <h1 className={style.title}>
         hello React
       </h1>
       <Button/>
